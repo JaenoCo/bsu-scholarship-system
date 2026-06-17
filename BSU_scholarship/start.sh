7f874d4a4d11447acc8563a73cff209c9e82dce4
JaenoCo <22-71318@g.batstate-u.edu.ph>
2026-06-17 10:50:43 +0800

    sdan

---
+++ BSU_scholarship/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Clearing cache..."
+php artisan config:clear
+php artisan cache:clear
+
+echo "Starting Laravel..."
+php artisan serve --host=0.0.0.0 --port=10000
